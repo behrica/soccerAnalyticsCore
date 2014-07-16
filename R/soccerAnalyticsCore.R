7d5b3f6323ce8a9e863bda83feb6213f380e0583
Carsten Behring <carsten.behring@gmail.com>
2014-07-16 11:17:21 +0200

    first version

---
+++ R/soccerAnalyticsCore.R
@@ -0,0 +1,48 @@
+library(RCurl)
+
+
+
+
+
+#'Loads the data
+#'@importFrom RCurl getBinaryURL
+#'@export
+#'@examples
+#'loadData()
+#'
+loadData <- function() {
+  read.csv(textConnection(rawToChar(getBinaryURL("https://raw.githubusercontent.com/codecentric/soccer-prediction-2014/master/1-merge-data/output/games.csv"))),sep = ";")
+  
+}
+
+
+#'Calculates stats for a given country
+#'
+#'@param country  A country string, such as "DEU"
+#'@param games The data loaded by 'loadData'
+#'@export
+#'@examples
+#'NA
+countryStats <- function(country,games)  {
+  results <- games[games$b_team_home==country | games$b_team_away==country ,c("b_team_home","b_team_away","r_goals_final_home","r_goals_final_away")]
+  results <- results[complete.cases(results),]
+  
+  results$points_home<-sign(results$r_goals_final_home - results$r_goals_final_away)+1
+  results$points_away<-sign(results$r_goals_final_away - results$r_goals_final_home)+1
+  
+  
+  goals.home <- results[results$b_team_home==country,"r_goals_final_home"]
+  goals.away <- results[results$b_team_away==country,"r_goals_final_away"]
+  
+  
+  
+  
+  points.home <- results[results$b_team_home==country,"points_home"]
+  points.away <- results[results$b_team_away==country,"points_away"]
+  
+  
+  goals <- c(goals.away,goals.home)
+  points <- factor(c(points.away,points.home),levels=c(0,1,2),labels = c("Lost","Draw","Won"))
+  
+  list(goals = goals,points=points)
+}
